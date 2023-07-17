7d7595a41c87e0de062e63c4cb8a9098496ec957
Fer <fernandogutromo@gmail.com>
2023-07-17 02:43:24 +0200

    Solucion final ejer 4

--- Project/ejer4.py
+++ Project/ejer4.py
@@ -1,9 +1,25 @@
 #Ejercicio 4 ---- Resolucion temporal
+#lista=[]
+#while True:
+#    lista.append(int(input("Escribe un numero: ")))
+#    confirmacion = input("¿Quieres continuar? Escribe s o n: ")
+#    if confirmacion == "n":
+#        break
+#media = sum(lista[0:])/len(lista)
+#print("La media es: " + str(media))
+
+#Ejercicio 4 ---- Solucion
+
 lista=[]
 while True:
-    lista.append(int(input("Escribe un numero: ")))
-    confirmacion = input("¿Quieres continuar? Escribe s o n: ")
-    if confirmacion == "n":
+    num = input("Escribe un numero (c para terminar): ")
+    if num == "c":
         break
-media = sum(lista[0:])/len(lista)
-print("La media es: " + str(media))
+    else:
+        lista.append(int(num))
+if len(lista) != 0:
+    media = round(sum(lista[0:])/len(lista), 3)
+    print("La media es: " + str(media))
+else:
+    print ("No hay datos")
+
